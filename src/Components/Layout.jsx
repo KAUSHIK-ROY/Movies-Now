49a04498d4b8cfe0fa65d2b68a975260b40ecdc8
KAUSHIK-ROY <kroy33387@gmail.com>
2024-09-10 03:06:25 +0530

    add responsive cards

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -7,15 +7,22 @@ import useFetch from '../Hooks/useFetch';
 
 export default function Layout() {
 
-  const {data : popularShow}=useFetch('/movie/popular')
-  const {data : upcoming}=useFetch('movie/upcoming')
+  const {data : popularShowData}=useFetch('/movie/popular')
+  const {data : upcomingData}=useFetch('movie/upcoming')
+  const {data : top_ratedData}=useFetch('/tv/top_rated')
+  const {data : now_playingData}=useFetch('/movie/now_playing')
+  const {data : airing_todayData}=useFetch('/tv/airing_today')
+  // const {data : Data}=useFetch('')
   return (
     <div className='layout'>
         <Promotion/>
         <SideNav/>
-        <Movies data={popularShow} heading={"Popular Movies"}/>
-        <Movies data={upcoming} heading={"Upcoming"}/>
+        <Movies data={popularShowData} heading={"Popular Movies"}/>
+        <Movies data={upcomingData} heading={"Upcoming"}/>
+        <Movies data={top_ratedData} heading={"Top Rated"}/>
+        <Movies data={now_playingData} heading={"Now Playing"}/>
+        <Movies data={airing_todayData} heading={"Airing Today"}/>
 
     </div>
   )
-}
+}  
--- src/Components/UI/Movies-Cards/Cards.jsx
+++ src/Components/UI/Movies-Cards/Cards.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './cards.css'
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Cards({data}) {
   const imageURL = useSelector(state => state.moviesData.imageURL)
+  console.log ("me",data)
   return (
     <div className="cards">
       <div className="v-card">
@@ -11,7 +15,18 @@ export default function Cards({data}) {
           data?.poster_path ?(<img src={imageURL+data?.poster_path}/>): 'not found'
         }
       </div>
-      {/* <h1>{data?.title || data?.name}</h1> */}
+      <div className="b-card">
+      {
+          data?.backdrop_path ?(<img src={imageURL+data?.backdrop_path}/>): 'not found'
+        }
+        <button ><FontAwesomeIcon icon={faPlay}/></button>
+        <h1>{data?.title || data?.name}</h1>
+        <p>
+          {data.original_language} | {data.release_date || data.first_air_date}
+        </p>
+        <p>{data?.overview}</p>
+      </div>
+
     </div>
   )
 }
